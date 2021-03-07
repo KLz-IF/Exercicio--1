3a033403bbacbddb452b42e859b88a51b4bbb8ba
Kathleen Luiza <kathleen.luiiza75@gmail.com>
2021-03-07 18:48:24 -0300

    Ex2

---
+++ Ex2.java
@@ -0,0 +1,14 @@
+pacote  Ex2 ;
+
+import  java.util.Scanner ;
+
+public  class  Ex2 {
+    public  static  void  main ( String [] args ) {
+        Leitura do scanner =  novo  scanner ( sistema . In);
+        int [] n =  novo  int [ 10 ];
+
+        para ( int i =  9 ; i > = 0 ; i - ) {
+            n [i] =  10 - i;
+        }
+    }
+}
