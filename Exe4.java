14d180c0d7c571f79687789f9f19b2d3efab708e
Kathleen Luiza <kathleen.luiiza75@gmail.com>
2021-03-07 18:49:52 -0300

    Exe4

---
+++ Exe4.java
@@ -0,0 +1,14 @@
+pacote  Ex4 ;
+
+public  class  Ex4 {
+    public  static  void  main ( String [] args ) {
+        int [] [] n =  novo  int [ 50 ] [ 100 ];
+
+        para ( int i =  99 ; i > = 0 ; i - ) {
+            n [ 0 ] [i] =  0 + i;
+        }
+        para ( int i =  49 ; i > = 0 ; i - ) {
+            n [i] [ 0 ] = i + 0 ;
+        }
+    }
+}
